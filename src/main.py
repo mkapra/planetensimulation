a63d13d15d915d141078c856aec5f1053ed4cdcd
Simon Meier <meier.simon@outlook.com>
2022-02-01 13:41:26 +0100

    Add container and statistics class

--- src/main.py
+++ src/main.py
@@ -1,4 +1,78 @@
 #!/usr/bin/python3
+import random
+from animals import Animal, Fish, Shark
+
+
+class Container(dict):
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a new game field
+        :param x: Amount of columns in the game field
+        :param y: Amount of rows in the game field
+        :param amount_fish: Amount of fishes at the start of the simulation
+        :param amount_sharks:
+        """
+        super(Container, self).__init__(*args, **kwargs)
+
+    def __missing__(self, *args, **kwargs):
+        """An empty cell returns value zero.
+
+        This is what lets us store a huge board and ignore dead cells.
+        An Array based implementation would be very space intensive and
+        expensive to iterate over."""
+        return 0
+
+
+class Statistics:
+    """
+    Class to keep statistics about the simulation
+    """
+
+    TOTAL_FISH = 'fishTotal'
+    TOTAL_SHARK = 'sharkTotal'
+    NEW_FISH = 'fishNew'
+    NEW_SHARK = 'sharkNew'
+    DIED_FISH = 'fishDied'
+    DIED_SHARK = 'sharkDied'
+    ALL_KEYS = {TOTAL_FISH, TOTAL_SHARK, NEW_FISH, NEW_SHARK, DIED_FISH, DIED_SHARK}
+
+    def __init__(self):
+        self._stats: dict[str, list[int]] = dict()
+        self.reset()
+
+    def reset(self):
+        self._stats.clear()
+        for key in self.ALL_KEYS:
+            self._stats[key] = []
+        self.add_cycle()
+
+    def get_stats(self):
+        return self._stats
+
+    def add_cycle(self):
+        for _, item in self._stats.items():
+            item.append(0)
+
+    def update_total(self, fish_total: int, shark_total: int):
+        self._stats.get(self.TOTAL_FISH)[-1] = fish_total
+        self._stats.get(self.TOTAL_SHARK)[-1] = shark_total
+
+    def update_from_stats(self, stats: dict[str, list[int]]):
+        for key, item in stats.items():
+            self._stats.get(key).extend(stats.get(key))
+
+    def shark_died(self):
+        self._stats.get(self.DIED_SHARK)[-1] += 1
+
+    def shark_born(self):
+        self._stats.get(self.NEW_SHARK)[-1] += 1
+
+    def fish_died(self):
+        self._stats.get(self.DIED_FISH)[-1] += 1
+
+    def fish_born(self):
+        self._stats.get(self.NEW_FISH)[-1] += 1
+
 
 if __name__ == "__main__":
-    print("Planetensimulation")
--- test/test_simulation.py
+++ test/test_simulation.py
@@ -1,4 +1,6 @@
+import random
 from unittest import TestCase
+from main import Container, Statistics, Simulation
 from animals import Animal, Fish, Shark
 
 
@@ -42,3 +44,82 @@ class TestAnimals(TestCase):
             self.shark.eats()
             self.shark.lives()
         self.assertFalse(self.shark.can_breed())
+
+
+class TestStatistics(TestCase):
+    def setUp(self) -> None:
+        self.stats = Statistics()
+        self.update = Statistics()
+
+    def test_init(self):
+        self.assertIsInstance(self.stats, Statistics)
+
+    def test_get_stats_empty(self):
+        stats_dict = self.stats.get_stats()
+        self.assertEqual(6, len(stats_dict))
+        for item in stats_dict.values():
+            self.assertEqual(0, item[0])
+
+    def test_shark_born(self):
+        self.stats.shark_born()
+        self.assertEqual(1, self.stats.get_stats().get(self.stats.NEW_SHARK)[0])
+
+    def test_shark_dies(self):
+        self.stats.shark_died()
+        self.assertEqual(1, self.stats.get_stats().get(self.stats.DIED_SHARK)[0])
+
+    def test_fish_born(self):
+        self.stats.fish_born()
+        self.assertEqual(1, self.stats.get_stats().get(self.stats.NEW_FISH)[0])
+
+    def test_fish_dies(self):
+        self.stats.fish_died()
+        self.assertEqual(1, self.stats.get_stats().get(self.stats.DIED_FISH)[0])
+
+    def test_stats_empty_reset(self):
+        self.stats.reset()
+        stats_dict = self.stats.get_stats()
+        self.assertEqual(6, len(stats_dict))
+        for item in stats_dict.values():
+            self.assertEqual(0, item[0])
+
+    def test_stats_get_stats(self):
+        self.assertIsInstance(self.stats.get_stats(), dict)
+
+    def test_stats_add_cycle(self):
+        for i in range(10):
+            ran = random.randint(0, 1000)
+            for j in range(ran):
+                self.stats.add_cycle()
+            self.assertEqual(ran + 1, len(self.stats.get_stats().get(Statistics.TOTAL_SHARK)))
+            self.stats.reset()
+
+    def test_stats_update_total(self):
+        for fishes, sharks in zip(range(random.randint(10, 1000)), range(random.randint(10, 1000))):
+            self.stats.update_total(fishes, sharks)
+            stats_dict = self.stats.get_stats()
+            self.assertEqual(fishes, stats_dict[Statistics.TOTAL_FISH][0])
+            self.assertEqual(sharks, stats_dict[Statistics.TOTAL_SHARK][0])
+
+    def test_stats_update_from_stats_empty(self):
+        self.stats.update_from_stats(self.update.get_stats())
+        stats_dict = self.stats.get_stats()
+        self.assertEqual(6, len(stats_dict))
+        for item in stats_dict.values():
+            self.assertEqual(0, item[0])
+
+    def test_stats_update_from_stats(self):
+        # create random stats in update
+        length = random.randint(10, 1000)
+        for cycles in range(length):
+            for key in Statistics.ALL_KEYS:
+                self.update._stats.get(key)[cycles] = random.randint(10, 1000)
+            if cycles < length - 1:
+                self.update.add_cycle()
+
+        # update statistics from update to stats
+        self.stats.update_from_stats(self.update.get_stats())
+        stats_dict = self.stats.get_stats()
+        for item in stats_dict.values():
+            self.assertEqual(length + 1, len(item))
+
